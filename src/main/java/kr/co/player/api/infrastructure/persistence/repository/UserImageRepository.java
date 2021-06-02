2b9456c3c137c4f345f4e86d9ac4d0d83b59f47b
hongyeongjune <hongyeongjune@naver.com>
2021-06-02 18:20:54 +0900

    오류 수정

--- src/main/java/kr/co/player/api/infrastructure/persistence/repository/UserImageRepository.java
+++ src/main/java/kr/co/player/api/infrastructure/persistence/repository/UserImageRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface UserImageRepository extends JpaRepository<UserImageRepository, Long> {
+public interface UserImageRepository extends JpaRepository<UserImageEntity, Long> {
     List<UserImageEntity> findByUserEntity(Long UserEntity);
 }
 
