d8077717362bc18e567a18dc1d38136225736d13
hongyeongjune <hongyeongjune@naver.com>
2021-06-04 17:49:07 +0900

    Repository 수정

--- src/main/java/kr/co/player/api/infrastructure/persistence/repository/UserImageRepositoryCustom.java
+++ src/main/java/kr/co/player/api/infrastructure/persistence/repository/UserImageRepositoryCustom.java
@@ -1,9 +1,13 @@
 package kr.co.player.api.infrastructure.persistence.repository;
 
+import kr.co.player.api.infrastructure.persistence.entity.UserEntity;
 import kr.co.player.api.infrastructure.persistence.entity.UserImageEntity;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserImageRepositoryCustom {
     Optional<UserImageEntity> fetchFirstOrderByIdDesc();
+    List<UserImageEntity> fetchByUserEntity(UserEntity userEntity);
+    Optional<UserImageEntity> fetchByUserEntityAndUrl(UserEntity userEntity, String url);
 }
--- src/main/java/kr/co/player/api/infrastructure/persistence/repository/impl/UserImageRepositoryCustomImpl.java
+++ src/main/java/kr/co/player/api/infrastructure/persistence/repository/impl/UserImageRepositoryCustomImpl.java
@@ -1,13 +1,16 @@
 package kr.co.player.api.infrastructure.persistence.repository.impl;
 
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import kr.co.player.api.infrastructure.persistence.entity.QUserEntity;
 import kr.co.player.api.infrastructure.persistence.entity.QUserImageEntity;
+import kr.co.player.api.infrastructure.persistence.entity.UserEntity;
 import kr.co.player.api.infrastructure.persistence.entity.UserImageEntity;
 import kr.co.player.api.infrastructure.persistence.repository.UserImageRepositoryCustom;
 import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
 
+import java.util.List;
 import java.util.Optional;
 
 public class UserImageRepositoryCustomImpl extends QuerydslRepositorySupport implements UserImageRepositoryCustom {
@@ -28,10 +31,28 @@ public class UserImageRepositoryCustomImpl extends QuerydslRepositorySupport imp
                 .fetchFirst());
     }
 
+    @Override
+    public List<UserImageEntity> fetchByUserEntity(UserEntity userEntity) {
+        return setFetchJoinQuery()
+                .where(eqUser(userEntity))
+                .fetch();
+    }
+
+    @Override
+    public Optional<UserImageEntity> fetchByUserEntityAndUrl(UserEntity userEntity, String url) {
+        return Optional.ofNullable(setFetchJoinQuery()
+                .where(eqUser(userEntity).and(this.userImage.url.eq(url)))
+                .fetchOne());
+    }
+
     private JPAQuery<UserImageEntity> setFetchJoinQuery() {
         return jpaQuery.select(this.userImage)
                 .from(this.userImage)
                 .innerJoin(this.userImage.userEntity, this.user)
                 .fetchJoin();
+    }
+
+    private BooleanExpression eqUser(UserEntity userEntity) {
+        return this.userImage.userEntity.id.eq(userEntity.getId());
     }
 }
