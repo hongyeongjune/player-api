8d1f18e8dd870238b3083e2cc4c008faa50ddd92
hongyeongjune <hongyeongjune@naver.com>
2021-06-09 09:52:57 +0900

    에러코드 및 yml 수정

--- src/main/java/kr/co/player/api/infrastructure/error/model/ErrorCode.java
+++ src/main/java/kr/co/player/api/infrastructure/error/model/ErrorCode.java
@@ -9,7 +9,10 @@ public enum  ErrorCode {
     UNAUTHORIZED_USER("해당 기능을 사용할 수 없습니다.", 403),
 
     //user
-    DUPLICATED_ID("아이디가 중복되었습니다.", 400);
+    DUPLICATED_ID("아이디가 중복되었습니다.", 400),
+
+    //club
+    DUPLICATED_CLUB("이름이 중복되었습니다.", 400);
 
     private String message;
     private int status;
