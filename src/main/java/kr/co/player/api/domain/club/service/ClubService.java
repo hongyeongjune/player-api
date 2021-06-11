b5b2b64a0118d6785e4ac17c6bd6d716f54fb033
hongyeongjune <hongyeongjune@naver.com>
2021-06-11 14:10:18 +0900

    Club Service 작성

---
+++ src/main/java/kr/co/player/api/domain/club/service/ClubService.java
@@ -0,0 +1,23 @@
+package kr.co.player.api.domain.club.service;
+
+import kr.co.player.api.domain.club.model.ClubDto;
+import kr.co.player.api.domain.shared.Address;
+import kr.co.player.api.infrastructure.persistence.entity.ClubEntity;
+import org.springframework.data.domain.Page;
+
+import java.util.List;
+
+public interface ClubService {
+
+    //create - integrated
+    ClubEntity createClub(ClubDto.CREATE create);
+
+    //read - integrated
+    Page<ClubEntity> getClubs(int pageNo);
+    ClubEntity getClub(String clubName);
+    Page<ClubEntity> getClubsByAddress(int pageNo, List<Address> addressList);
+    Page<ClubEntity> getClubsByKeywordContains(int pageNo, String keyword);
+
+    //update - integrated
+    void updateClub(ClubEntity clubEntity, ClubDto.UPDATE update);
+}
---
+++ src/main/java/kr/co/player/api/domain/club/service/ClubServiceImpl.java
@@ -0,0 +1,64 @@
+package kr.co.player.api.domain.club.service;
+
+import kr.co.player.api.domain.club.model.ClubDto;
+import kr.co.player.api.domain.shared.Address;
+import kr.co.player.api.domain.shared.PageUtil;
+import kr.co.player.api.infrastructure.error.exception.DuplicatedException;
+import kr.co.player.api.infrastructure.error.exception.NotFoundException;
+import kr.co.player.api.infrastructure.error.model.ErrorCode;
+import kr.co.player.api.infrastructure.persistence.entity.ClubEntity;
+import kr.co.player.api.infrastructure.persistence.repository.ClubRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ClubServiceImpl implements ClubService {
+
+    private final ClubRepository clubRepository;
+
+    private final static int LIMIT = 10;
+
+    @Override
+    public ClubEntity createClub(ClubDto.CREATE create) {
+
+        if(clubRepository.existsByClubName(create.getName())) {
+            throw new DuplicatedException(ErrorCode.DUPLICATED_ID);
+        }
+
+        return clubRepository.save(ClubEntity.builder()
+                .clubName(create.getName())
+                .address(new Address(create.getCity(), create.getDistrict()))
+                .rating(0)
+                .build());
+    }
+
+    @Override
+    public Page<ClubEntity> getClubs(int pageNo) {
+        return clubRepository.fetchClubs(PageUtil.applyPageConfig(pageNo, LIMIT));
+    }
+
+    @Override
+    public ClubEntity getClub(String clubName) {
+        return clubRepository.findByClubName(clubName).orElseThrow(() -> new NotFoundException("ClubEntity"));
+    }
+
+    @Override
+    public Page<ClubEntity> getClubsByAddress(int pageNo, List<Address> addressList) {
+        return clubRepository.fetchClubsByAddress(PageUtil.applyPageConfig(pageNo, LIMIT), addressList);
+    }
+
+    @Override
+    public Page<ClubEntity> getClubsByKeywordContains(int pageNo, String keyword) {
+        return clubRepository.fetchClubsByKeywordContains(PageUtil.applyPageConfig(pageNo, LIMIT), keyword);
+    }
+
+    @Override
+    public void updateClub(ClubEntity clubEntity, ClubDto.UPDATE update) {
+        clubEntity.update(update);
+        clubRepository.save(clubEntity);
+    }
+}
