13c873d65cffcb43efbd81ca9e9ff17015330d2c
hongyeongjune <hongyeongjune@naver.com>
2021-08-25 16:18:51 +0900

    MatchStatus 작성

---
+++ src/main/java/kr/co/player/api/domain/match/model/common/MatchStatus.java
@@ -0,0 +1,21 @@
+package kr.co.player.api.domain.match.model.common;
+
+import kr.co.player.api.infrastructure.error.exception.UserDefineException;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.Arrays;
+
+@Getter
+@AllArgsConstructor
+public enum MatchStatus {
+    RECRUITMENT("대기중"), PROCEEDING("진행중"), FINISH("종료");
+
+    private String matchStatus;
+
+    public static MatchStatus of(String matchStatus) {
+        return Arrays.stream(MatchStatus.values())
+                .filter(status -> status.toString().equalsIgnoreCase(matchStatus))
+                .findAny().orElseThrow(() -> new UserDefineException("MatchStatus 항목을 찾을 수 없습니다."));
+    }
+}
